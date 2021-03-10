01756a13778983768d2128084538c48fbe204833
DanielRamosHoogwout <73166701+DanielRamosHoogwout@users.noreply.github.com>
2021-03-10 23:05:55 +0100

    Update ExportTurism.R

--- Scrips/ExportTurism.R
+++ Scrips/ExportTurism.R
@@ -7,6 +7,7 @@ library(lubridate)
 #Cargamos datasets mercados
 df3_mad <- read.csv("../data/Dataset3a_Datos_MercaMadrid.txt", sep = "|", dec = ",")
 df3_bcn <- read.csv("../data/Dataset3b_Datos_MercaBarna.txt", sep = "|", dec = ",")
+df2 <- read.csv("../data/Dataset2.- Precios Semanales Observatorio de Precios Junta de Andalucia.txt", sep = "|", dec = ",")
 
 #Cargamos datasets numero turístas
 df_tur <- read.csv("../data/nturistas_comunidad.csv", sep = ";", dec=",")
@@ -49,6 +50,123 @@ volume_bcn <- df3_bcn %>%
   group_by(YEAR, MONTH) %>%
   summarise(volume =sum(Volumen))
 
+#Unimos madrid y covid
 all_mad <- left_join(volume_mad, covid_spain)
 all_mad <- all_mad %>% 
   mutate(fecha = make_date(year = YEAR, month = MONTH))
+#Cambiamos NA por 0
+all_mad$casos <- ifelse(is.na(all_mad$casos),0,all_mad$casos)
+#Normalizamos entre 0 y 1
+all_mad$volume <- (all_mad$volume-min(all_mad$volume))/(max(all_mad$volume)-min(all_mad$volume))
+all_mad$casos <- (all_mad$casos-min(all_mad$casos))/(max(all_mad$casos)-min(all_mad$casos))
+
+all_mad %>% ggplot(aes(x=fecha, y=volume)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = casos))
+
+#Unimos madrid y turistas
+df_tur <- df_tur %>%
+  filter(Comunidades.autónomas=="Total")
+all_tur <- left_join(volume_mad, df_tur)
+all_tur <- all_tur %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+
+#Normalizamos entre 0 y 1
+all_tur$volume <- (all_tur$volume-min(all_tur$volume))/(max(all_tur$volume)-min(all_tur$volume))
+all_tur$Total <- (all_tur$Total-min(all_tur$Total))/(max(all_tur$Total)-min(all_tur$Total))
+
+all_tur %>% ggplot(aes(x=fecha, y=volume)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = Total))
+cor(all_tur$volume,all_tur$Total)
+
+#Dataset 2
+
+cebolla <- df2 %>% filter(PRODUCTO=="CEBOLLA", POSICION=="Mercas")
+cebolla$FIN <- parse_date(cebolla$FIN, "%d/%m/%Y")
+cebolla <- cebolla %>%
+  mutate(MONTH = month(FIN), YEAR = year(FIN))
+
+cebolla <- cebolla %>%
+  group_by(MONTH,YEAR) %>%
+  summarise(precio = mean(PRECIO))
+
+#Precio cebolla vs Mercamadrid
+cebolla_mad <- left_join(volume_mad, cebolla)
+cebolla_mad <- cebolla_mad %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+cebolla_mad$volume <- (cebolla_mad$volume-min(cebolla_mad$volume))/(max(cebolla_mad$volume)-min(cebolla_mad$volume))
+cebolla_mad$precio <- (cebolla_mad$precio-min(cebolla_mad$precio))/(max(cebolla_mad$precio)-min(cebolla_mad$precio))
+
+cebolla_mad %>% ggplot(aes(x=fecha, y=volume)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = precio))
+
+#Precio cebolla vs covid
+cebolla_covid <- left_join(cebolla, covid_spain)
+cebolla_covid$casos <- ifelse(is.na(cebolla_covid$casos),0,cebolla_covid$casos)
+cebolla_covid <- cebolla_covid %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+
+cebolla_covid$casos <- (cebolla_covid$casos-min(cebolla_covid$casos))/(max(cebolla_covid$casos)-min(cebolla_covid$casos))
+cebolla_covid$precio <- (cebolla_covid$precio-min(cebolla_covid$precio))/(max(cebolla_covid$precio)-min(cebolla_covid$precio))
+
+cebolla_covid %>% ggplot(aes(x=fecha, y=precio)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = casos))
+#######################################################################
+
+cebolla <- df2 %>% filter(PRODUCTO=="AGUACATE", POSICION=="Mercas")
+cebolla$FIN <- parse_date(cebolla$FIN, "%d/%m/%Y")
+cebolla <- cebolla %>%
+  mutate(MONTH = month(FIN), YEAR = year(FIN))
+
+cebolla <- cebolla %>%
+  group_by(MONTH,YEAR) %>%
+  summarise(precio = mean(PRECIO))
+
+#Precio cebolla vs Mercamadrid
+cebolla_mad <- left_join(volume_mad, cebolla)
+cebolla_mad <- cebolla_mad %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+cebolla_mad$volume <- (cebolla_mad$volume-min(cebolla_mad$volume))/(max(cebolla_mad$volume)-min(cebolla_mad$volume))
+cebolla_mad$precio <- (cebolla_mad$precio-min(cebolla_mad$precio))/(max(cebolla_mad$precio)-min(cebolla_mad$precio))
+
+cebolla_mad %>% ggplot(aes(x=fecha, y=volume)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = precio))
+
+#Precio cebolla vs covid
+cebolla_covid <- left_join(cebolla, covid_spain)
+cebolla_covid$casos <- ifelse(is.na(cebolla_covid$casos),0,cebolla_covid$casos)
+cebolla_covid <- cebolla_covid %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+
+cebolla_covid$casos <- (cebolla_covid$casos-min(cebolla_covid$casos))/(max(cebolla_covid$casos)-min(cebolla_covid$casos))
+cebolla_covid$precio <- (cebolla_covid$precio-min(cebolla_covid$precio))/(max(cebolla_covid$precio)-min(cebolla_covid$precio))
+
+cebolla_covid %>% ggplot(aes(x=fecha, y=precio)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = casos))
+
+##################################
+#Turismo covid OJO CON EL DF_TUR tiene que estar recien cargado
+df_tur %<>% filter(Comunidades.autónomas=="04 Balears, Illes")
+tur_covid <- left_join(df_tur, covid_spain)
+tur_covid$casos <- ifelse(is.na(tur_covid$casos),0,tur_covid$casos)
+tur_covid <- tur_covid %>% 
+  mutate(fecha = make_date(year = YEAR, month = MONTH))
+
+tur_covid$casos <- (tur_covid$casos-min(tur_covid$casos))/(max(tur_covid$casos)-min(tur_covid$casos))
+tur_covid$Total <- (tur_covid$Total-min(tur_covid$Total))/(max(tur_covid$Total)-min(tur_covid$Total))
+
+tur_covid %>% ggplot(aes(x=fecha, y=Total)) +
+  geom_line(col= 'red')+
+  geom_point() +
+  geom_line(aes(y = casos))
